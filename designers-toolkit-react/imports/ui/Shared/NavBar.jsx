a28da72afbbc298ded083e38f85420a218130495
Augustas <43470011+AugustasFriend@users.noreply.github.com>
2021-11-24 16:59:07 +0100

    fix vertically aligned navbar

--- designers-toolkit-react/imports/ui/Shared/NavBar.jsx
+++ designers-toolkit-react/imports/ui/Shared/NavBar.jsx
@@ -6,10 +6,10 @@ import Col from 'react-bootstrap/Col';
 
 
 export const NavBar = () => (
-  <div className='navContainer'>
+  <div className="navContainer">
     <Container>
-      <Row className='navRow'>
-      <Col><Link to="/" class="navText"><img src='logo.png'/></Link></Col>
+      <Row>
+      <Col className ='navLink'><Link to="/" class="navText"><img src='logo.png'/></Link></Col>
         <Col><Link to="/introduction" class="navText">Introduction</Link></Col>
         <Col><Link to="/methodologies" class="navText">Methodologies</Link></Col>
         <Col><Link to="/currentplan" class="navText">Current Plan</Link></Col>
