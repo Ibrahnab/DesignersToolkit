d64381ff7bb4a50e1bca29ec3d506148d4655a97
Augustas <43470011+AugustasFriend@users.noreply.github.com>
2021-11-24 16:26:48 +0100

    add logo

--- designers-toolkit-react/imports/ui/Shared/NavBar.jsx
+++ designers-toolkit-react/imports/ui/Shared/NavBar.jsx
@@ -9,7 +9,7 @@ export const NavBar = () => (
   <div className='navContainer'>
     <Container>
       <Row className='navRow'>
-      <Col><Link to="/" class="navText">Dashboard</Link></Col>
+      <Col><Link to="/" class="navText"><img src='logo.png'/></Link></Col>
         <Col><Link to="/introduction" class="navText">Introduction</Link></Col>
         <Col><Link to="/methodologies" class="navText">Methodologies</Link></Col>
         <Col><Link to="/currentplan" class="navText">Current Plan</Link></Col>
