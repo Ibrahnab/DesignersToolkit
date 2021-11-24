b91fb279653269c04dc57d9975458aa5d7139919
Augustas <43470011+AugustasFriend@users.noreply.github.com>
2021-11-24 15:01:42 +0100

    nothing work

--- designers-toolkit-react/imports/ui/Shared/NavBar.jsx
+++ designers-toolkit-react/imports/ui/Shared/NavBar.jsx
@@ -9,7 +9,7 @@ export const NavBar = () => (
   <div className='navContainer'>
     <Container>
       <Row className='navRow'>
-        <Col><Link to="/">Dashboard</Link></Col>
+        <Col><Link to="/"><div className='logoDiv'></div></Link></Col>
         <Col><Link to="/introduction">Introduction</Link></Col>
         <Col><Link to="/methodologies">Methodologies</Link></Col>
         <Col><Link to="/currentplan">Current Plan</Link></Col>
