dd93b78d2f7b6476550ebcd2c3884f2bd3f5d42b
Augustas <43470011+AugustasFriend@users.noreply.github.com>
2021-11-25 17:49:56 +0100

    add elements to dashboard page
    add textfields from imported library. add text and fix positioning

--- designers-toolkit-react/imports/ui/Dashboard/AccountSide.jsx
+++ designers-toolkit-react/imports/ui/Dashboard/AccountSide.jsx
@@ -1,8 +1,34 @@
 import React from 'react';
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import {UsernameTextField} from './UsernameTextField';
+import {PasswordTextField} from './PasswordTextField';
 
 export const AccountSide = () => (
   
     <div>
-        <p className ="dashboardHeader">Create or select a project</p>
+        <Container>
+            <Row>
+                <Col>
+                <p className ="dashboardHeader">Create or select a project</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <p className ='dashboardStandardText'>You are not signed in, if you want to create projects, please sign in or create a new account
+</p>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                <UsernameTextField/>
+                </Col>
+                <Col>
+                    <PasswordTextField/>
+                </Col>
+            </Row>
+        </Container>
+        
     </div>
   ); 
--- designers-toolkit-react/imports/ui/Dashboard/RoamMode.jsx
+++ designers-toolkit-react/imports/ui/Dashboard/RoamMode.jsx
@@ -1,15 +1,34 @@
 import React from "react";
-
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
 
 export const RoamMode = () => (
   
     <div>
-        <p className ="dashboardHeader">Use without an account</p>
-        <img className ='dashboardCardImage'
-             src='dashboard_cards_image.svg'/>
-        <button className='dashboardStartButton'><img className='dashboardButtonImage' src='dashboard_roam_button.svg' onClick={myFunc}/></button>
-        <p className='dashboardRoamPara'>You can only search for and browse through methodologies in this mode. If you want to create projects and save your progress, 
-            please sign in or create a new account on the right hand side.</p>
+        <Container>
+            <Row>
+                <Col>
+                <p className ="dashboardHeader">Use without an account</p>
+                </Col>
+            </Row>
+            <Row className="dashboardImageRow">
+                <Col>
+                    <img className ='dashboardCardImage'src='dashboard_cards_image.svg'/>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <button className='dashboardStartButton'><img className='dashboardButtonImage' src='dashboard_roam_button.svg' onClick={myFunc}/></button>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                <p className='dashboardStandardText'>You can only search for and browse through methodologies in this mode. If you want to create projects and save your progress, 
+                please sign in or create a new account on the right hand side.</p>
+                </Col>
+            </Row>
+        </Container>
     </div>
   ); 
 
