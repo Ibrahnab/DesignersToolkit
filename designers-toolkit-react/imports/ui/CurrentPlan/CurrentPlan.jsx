bd2e727d43d7132a787a727d9650496defd4a7c7
Augustas <43470011+AugustasFriend@users.noreply.github.com>
2021-11-29 01:33:21 +0100

    ad img

--- designers-toolkit-react/imports/ui/CurrentPlan/CurrentPlan.jsx
+++ designers-toolkit-react/imports/ui/CurrentPlan/CurrentPlan.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 
 export const CurrentPlan = () => (
     <div>
-      <h1>Current Plan Page</h1>
+      <Container>
+        <Row>
+          <Col md="auto">
+            <img src="small_triplediamond.svg"/>
+          </Col>
+        </Row>
+      </Container>
     </div>
   ); 
