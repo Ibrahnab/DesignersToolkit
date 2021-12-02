346ea28417e69f7cf5bf9a4e731c23337a635f65
Ibrahnab <ibrahim.naboulsi97@gmail.com>
2021-12-02 22:46:25 +0100

    created triple diamond nav

--- designers-toolkit-react/imports/ui/Methodologies/Methodologies.jsx
+++ designers-toolkit-react/imports/ui/Methodologies/Methodologies.jsx
@@ -4,32 +4,69 @@ import MethodCard from "../Shared/MethodCard";
 import {connect} from "react-redux"
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-
-// export const Methodologies = ({methods}) => (
-  
-//     <div>
-//       <h1>Methodologies Page</h1>
-//       <div>
-//         {methods.map((meth) => (
-//            <MethodCard /> 
-//         ))}
-//       </div>
-      
-//       </div>
-//   ); 
+import Container from "react-bootstrap/Container";
 
 const Methodologies = ({methods}) => {
   return (
     <div>
-      <h1>Methodologies Page</h1>
-      <Row>
-      {methods.map((meth) => (
-        <Col md="auto">
-            <MethodCard key={meth.id} methodData={meth} /> 
-            </Col>
-         ))}
-         </Row>
-       </div>
+      <Container className="auto">
+
+      <Container className="diamondContainer">
+        <Row>
+          <Col md="auto" className="fluid triangleBtn d-flex justify-content-around p-0">
+              <div class="triangleLeft understandTriangle">
+                  <p className="whiteHeader">Phase 1 Understand
+                  </p>
+              </div>
+          </Col>
+          <Col md="auto" className="fluid triangleBtn d-flex justify-content-around p-0">
+            <div class="triangleRight defineTriangle">
+                <p className="whiteHeader">Phase 2 Define
+                </p>
+              </div>
+          </Col>
+          
+          <Col md="auto" className="fluid triangleBtn d-flex p-0 justify-content-around p-0">
+            <div class="triangleLeft sketchTriangle">
+              <p className="whiteHeader">Phase 3 Sketch
+              </p>
+            </div>
+          </Col>
+
+          <Col md="auto" className="fluid triangleBtn d-flex justify-content-around p-0">
+            <div class="triangleRight decideTriangle">
+                <p className="whiteHeader">Phase 4 Decide
+                </p>
+              </div>
+          </Col>
+
+          <Col md="auto" className="fluid triangleBtn d-flex justify-content-around p-0">
+              <div class="triangleLeft prototypeTriangle">
+                  <p className="whiteHeader">Phase 5 Prototype
+                  </p>
+              </div>
+          </Col>
+          <Col md="auto" className="fluid triangleBtn d-flex justify-content-around p-0">
+            <div class="triangleRight validateTriangle">
+                <p className="whiteHeader">Phase 6 Validate
+                </p>
+              </div>
+          </Col>
+          
+          
+        </Row>
+      </Container>
+        {/*------------ Not supposed to be in the final ---------- */}
+        <Row>
+          {methods.map((meth) => (
+              <Col md="auto">
+                <MethodCard key={meth.id} methodData={meth} /> 
+              </Col>
+              ))
+          }
+        </Row>
+      </Container>
+    </div>
   );
 };
 
