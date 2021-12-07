8084088b41353aedef30fdd0538f55351116273f
Ibrahnab <ibrahim.naboulsi97@gmail.com>
2021-12-07 11:10:51 +0100

    defined checkboxes and added searchbar

--- designers-toolkit-react/imports/ui/Shared/HamburgerMenu.jsx
+++ designers-toolkit-react/imports/ui/Shared/HamburgerMenu.jsx
@@ -14,7 +14,22 @@ import { FiMenu } from "react-icons/fi"
 
 const HamburgerMenu = ({methods}) => {
 
-    const [navbarOpen, setNavbarOpen] = useState(false)
+    const [navbarOpen, setNavbarOpen] = useState(false);
+    const [search, setSearchState] = useState("understand");
+    const [timeState, setTimeState] = useState(50);
+    const [participantsState, setParticipantsState] = useState(1);
+    const [checkedState, setCheckedState] = useState(
+      new Array(6).fill(false)
+    );
+    const handleOnChange = (position) => {
+      const updatedCheckedState = checkedState.map((item, index) =>
+        index === position ? !item : item
+      );
+    
+      setCheckedState(updatedCheckedState);
+
+    };
+
 
     const handleToggle = () => {
         setNavbarOpen(prev => !prev)
@@ -23,6 +38,8 @@ const HamburgerMenu = ({methods}) => {
       const closeMenu = () => {
         setNavbarOpen(false)
       }
+
+      
 
     return (
         <div className="navBar2">
@@ -35,28 +52,38 @@ const HamburgerMenu = ({methods}) => {
                 <Col sm="10" className="d-flex"><h5 className="blackHeader">Search for design methods</h5></Col>
                 <Col sm="1"><img src="crossIcon.svg"></img></Col>
               </Row>
+
+              <Row>
+                <Col>
+                  <Form.Control type="text" placeholder="Normal text" onChange={(e)=> setSearchState(e.target.value)} />
+                </Col>
+              </Row>
+
+              <Row><Col><hr></hr></Col></Row>
+
               <Row className="firstSearchRow">
                 <Col>
                   <p className="smallBlackHeader">Select phases</p>
                 </Col>
                 
               </Row>
+              
               <Row>
                     <Col>
-                      <Form.Check type="checkbox" inline label="Understand" name="group1" />
-                      <Form.Check type="checkbox" inline label="Define" name="group1" />
+                      <Form.Check type="checkbox" inline label="Understand" name="group1" checked={checkedState[1]} onChange={() => handleOnChange(1)} />
+                      <Form.Check type="checkbox" inline label="Define" name="group1"  checked={checkedState[2]} onChange={() => handleOnChange(2)}/>
                     </Col>
                   </Row>
                   <Row>
                     <Col>
-                      <Form.Check type="checkbox" inline label="Sketch" name="group1" />
-                      <Form.Check type="checkbox" inline label="Decide" name="group1" />
+                      <Form.Check type="checkbox" inline label="Sketch" name="group1"  checked={checkedState[3]} onChange={() => handleOnChange(3)}/>
+                      <Form.Check type="checkbox" inline label="Decide" name="group1"  checked={checkedState[4]} onChange={() => handleOnChange(4)} />
                     </Col>
                   </Row>
                   <Row>
                     <Col>
-                      <Form.Check type="checkbox" inline label="Prototype" name="group1" />
-                      <Form.Check type="checkbox" inline label="Validate" name="group1" />
+                      <Form.Check type="checkbox" inline label="Prototype" name="group1"  checked={checkedState[5]} onChange={() => handleOnChange(5)} />
+                      <Form.Check type="checkbox" inline label="Validate" name="group1" checked={checkedState[6]} onChange={() => handleOnChange(6)} />
                     </Col>
                   </Row>
 
@@ -74,12 +101,13 @@ const HamburgerMenu = ({methods}) => {
                   <p className="smallBlackHeader">Amount of participants</p>
                 </Col>
                 <Col>
-                  <input className="inputNumber" type="number"></input>
+                  <input className="inputNumber" type="number" value={participantsState} onChange={(e) => setParticipantsState(e.target.value)}></input>
                 </Col>
               </Row>
+              <Row><Col><hr></hr></Col></Row>
 
               <Row className="firstSearchRow">
-              {methods.filter(meth => meth.phase.find(ph => ph === "define")).map((meth) => (
+              {methods.filter(meth => meth.phase.find(ph => ph == search)).map((meth) => (
                 
                 <Col md="auto">
                   <MethodCard key={meth.id} methodData={meth} /> 
